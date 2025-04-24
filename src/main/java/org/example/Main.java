34a052258feda9a87cff5b4e5ebaf63f8b20d977
siwoo <k74415834@gmail.com>
2025-04-24 23:37:50 +0900

    드디어dks되18 file 된얀야딘ㄴㅇ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
 package org.example;
 
+/**
+ * You must document class
+ */
 public class Main {
+	/**
+	 * You must document method
+	 *
+	 * @param args You must document parameters
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("Hello, World!");
